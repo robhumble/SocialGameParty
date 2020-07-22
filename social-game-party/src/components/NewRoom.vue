35bd66af921e86054be709a18ac8e97abe2f1236
Ben <benjamin.l.humble@gmail.com>
2020-07-22 16:46:34 -0500

    #6 Created section to navigate rooms, doesn't yet touch database

---
+++ social-game-party/src/components/NewRoom.vue
@@ -0,0 +1,88 @@
+<!-- This component lets the user choose what room they will join or make. It doesn't actually hit the database yet. Join pretends it works.
+it was originally going to be hidden after joining a room, but it is currently our only navigation, so I left it persistent.-->
+<template>
+    <div>
+        <h1 v-if=!clientInRoom>You are not in a game room!</h1>
+        <h1 v-if=clientInRoom>You are in room: {{clientInRoom}}</h1>
+
+        <h3>Would you like to:</h3>
+        <!-- prevent loading both options at once by making the other button's value false -->
+        <v-btn @click="joinARoom = true, makeARoom = false">Join a room!</v-btn>
+        <span style="font-weight:bold">Or</span>
+        <v-btn @click="makeARoom = true, joinARoom = false">Make a new room!</v-btn>
+        <span v-if=clientInRoom style="font-weight:bold">Or</span>
+        <v-btn v-if=clientInRoom @click="exitRoom(), joinARoom=false, makeARoom=false">Exit!</v-btn>
+
+        <!-- forms for joining and making rooms respectively -->
+        <div v-if=joinARoom>
+            <h3>What is your room name?</h3>
+            <v-container align-center>
+                <v-row>
+                    <v-col>
+                        <v-text-field
+                            outlined
+                            v-model="joinRoomName"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col @click=joinRoom(joinRoomName)><v-btn>Join</v-btn></v-col>
+                </v-row>
+            </v-container>  
+        </div>
+        <div v-if=makeARoom>
+            <h3>What will you name your room?</h3>
+            <v-container align-center>
+                <v-row>
+                    <v-col>
+                        <v-text-field
+                            outlined
+                            v-model="makeRoomName"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col @click=makeRoom(makeRoomName)><v-btn>Make</v-btn></v-col>
+                </v-row>
+            </v-container>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+//import DataConnector from "@/logic/DataConnector.js";
+//actual database interaction pending, since the database doesn't know we're in a room without a user concept.
+
+export default {
+    data() {return{
+        clientInRoom: "",
+        joinARoom: false,
+        makeARoom: false,
+        joinRoomName: "",
+        makeRoomName: ""
+
+    }},
+    methods: {
+        makeRoom() {
+
+            this.joinARoom=false;
+            this.makeARoom=false;
+            this.joinRoomName="";
+            this.makeRoomName="";
+        },
+
+        joinRoom(joinRoomName) {
+            this.clientInRoom=joinRoomName;
+            this.joinARoom=false;
+            this.makeARoom=false;
+            this.joinRoomName="";
+            this.makeRoomName="";
+        },
+        exitRoom(){
+            this.clientInRoom="";
+            this.joinARoom=false;
+            this.makeARoom=false;
+            this.joinRoomName="";
+            this.makeRoomName="";
+        }
+    }
+  }  
+</script>
--- social-game-party/src/views/Home.vue
+++ social-game-party/src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <!-- ===== Page Content ===== -->
+
+    <!-- Room navigation. Still in progress. -->
     <v-container>
-      <v-row class="text-center">
+      <v-row class="text-center">  
+        <v-col>
+          <NewRoom></NewRoom>
+        </v-col>
+      </v-row>
+    </v-container>
+    
+    <v-container>
+      <v-row class="text-center">      
         <v-col cols="12">
           <v-btn v-if="!inChatRoom" @click="inChatRoom = true">Enter Chat Room</v-btn>
           <v-btn v-if="inChatRoom" @click="inChatRoom = false">Exit Chat Room</v-btn>
@@ -16,10 +26,12 @@
 <script>
 // @ is an alias to /src
 import ChatRoom from "@/components/ChatRoom.vue";
+import NewRoom from "@/components/NewRoom.vue";
 
 export default {
   name: "Home",
   components: {
+    NewRoom,
     ChatRoom
   },
   data: () => ({
